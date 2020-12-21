ea90a021a5f5b3402e802c82d3a9ce6a2e69f1af
Asya02 <74601520+Asya02@users.noreply.github.com>
2020-12-21 22:56:12 +0300

    Update queue.py

--- queue.py
+++ queue.py
@@ -3,48 +3,51 @@ class Element:
         self.data = data
         self.next = None
         self.prev = None
+        
 
 
 class Queue:
-    def __init__(self):
+    def __init__(self, size):
         self.front = None
         self.back = None
+        self.size = 0
 
 # Добавление данных элемента в очередь
-    def enqueue(self, data):
+    def enqueue(self, data, size):
+        element = Element(data)
+        size += 1
         if self.back is None:
-            self.front = Element(data)
+            self.front = element
             self.back = self.front
         else:
-            self.back.next = Element(data)
+            self.back.next = element
             self.back.next.prev = self.back
             self.back = self.back.next
 
 # Удаление первого элемента из очереди
-    def dequeue(self):
+    def dequeue(self, size):
         if self.front is None:
             return None
         else:
+            size -= 1
             temp = self.front.data
             self.front = self.front.next
             self.front.prev = None
             return temp
 
 # Возврат верхнего элемента в очередь
-    def first(self):
-        return self.front.data
+    def first(self, size):
+        if size == 0:
+            return None
+        else:
+            return self.front.data
 
 # Размер очереди
-    def size(self):
-        temp = self.front
-        count = 0
-        while temp is not None:
-            count = count+1
-            temp = temp.next
-        return count
+    def size(self, size):
+        return size
 
 # Очистить очередь
     def clear(self):
         self.front = None
         self.back = None
-        self.size = None
+        size = 0
