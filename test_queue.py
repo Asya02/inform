d264bb804ce0fdf7942734988fac19d40ec7fd07
Asya02 <74601520+Asya02@users.noreply.github.com>
2020-12-22 20:54:43 +0300

    Update test_queue.py

--- test_queue.py
+++ test_queue.py
@@ -1,57 +1,42 @@
 import unittest
 from queue import Queue
 
+
 class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.test_queue = Queue()
-        
+
     def test_enqueue(self):
         self.assertEqual(self.test_queue.enqueue(1), 1)
-
+        self.assertEqual(self.test_queue.size, 1)
 
     def test_dequeue(self):
         self.assertEqual(self.test_queue.dequeue(), None)
+        self.assertEqual(self.test_queue.size, 0)
 
     def test_dequeue2(self):
         self.test_queue.enqueue(1)
         self.test_queue.enqueue(2)
-        self.test_queue.enqueue(3)
-        self.test_queue.dequeue()
-        self.assertEqual(self.test_queue, 23)
+        self.assertEqual(self.test_queue.dequeue(), 2)
+        self.assertEqual(self.test_queue.size, 1)
 
     def test_first(self):
-        self.test_queue.first()
-        self.assertEqual(self.test_queue, None)
+        self.assertEqual(self.test_queue.first(), None)
+        self.assertEqual(self.test_queue.size, 0)
 
     def test_first2(self):
         self.test_queue.enqueue(1)
-        self.test_queue.enqueue(2)
-        self.test_queue.enqueue(3)
-        self.test_queue.first()
-        self.assertEqual(self.test_queue, 1)
-
-    def test_size(self):
-        self.test_queue.enqueue(1)
-        self.test_queue.enqueue(2)
-        self.test_queue.enqueue(3)
-        self.test_queue.size()
-        self.assertEqual(self.test_queue, 3)
-        
-    def test_size2(self):
-        self.test_queue.size()
-        self.assertEqual(self.test_queue, 0)
+        self.assertEqual(self.test_queue.first(), 1)
+        self.assertEqual(self.test_queue.size, 1)
 
     def test_clear(self):
-        self.test_queue.clear()
-        self.assertEqual(self.test_queue, None)
+        self.assertEqual(self.test_queue.clear(), None)
+        self.assertEqual(self.test_queue.size, 0)
 
     def test_clear2(self):
         self.test_queue.enqueue(1)
-        self.test_queue.enqueue(2)
-        self.test_queue.enqueue(3)
-        self.test_queue.clear
-        self.assertEqual(self.test_queue, None)
+        self.assertEqual(self.test_queue.clear(), None)
+        self.assertEqual(self.test_queue.size, 0)
 
-        
 if __name__ == '__main__':
     unittest.main()
